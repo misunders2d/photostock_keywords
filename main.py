698a787c546e8445e10a487ff3b6115fcce79584
MISUNDERSTOOD <2djohar@gmail.com>
2023-03-26 17:27:48 +0300

    testing

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import json
 
 def get_keywords(stock, url, joins):
     HEADERS = {
-        'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36","Accept-Language": "en-US, en;q=0.5"
+        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OSX 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/71.0.3578.98 Safari/537.36",
+        "Accept":"text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,image/apng,*/*;q=0.8"
         }
     page = requests.get(url, headers = HEADERS)
     if page.status_code != 200:
